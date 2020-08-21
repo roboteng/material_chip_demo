0b9b798c4aded1fc9e78fe83b60587b728badd26
Trevor Settles <roboteng@gmail.com>
2020-08-21 16:43:26 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/trevorsettles/material_chip_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trevorsettles.material_chip_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> labels;
+  String selected;
+
+  @override
+  void initState() {
+    super.initState();
+    labels = ['All', 'None', 'One', 'Two', 'Lunch', 'Dinner'];
+    selected = labels[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Chip Demo'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Semantics(
+            label: 'Items',
+            customSemanticsActions: {
+              for (int i = 0; i < labels.length; i++)
+                CustomSemanticsAction(
+                        label:
+                            '${labels[i]}${selected == labels[i] ? ', Selected' : ''}'):
+                    () {
+                  setState(() {
+                    selected = labels[i];
+                  });
+                }
+            },
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: labels
+                    .map<Widget>(
+                      (e) => Semantics(
+                        selected: selected == e,
+                        button: true,
+                        child: FlatButton(
+                          key: ValueKey(e),
+                          onPressed: () {
+                            setState(() {
+                              selected = e;
+                            });
+                          },
+                          child: Chip(
+                            label: Text(e),
+                            backgroundColor:
+                                selected == e ? Colors.blue[700] : null,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ),
+          Text('Content content content'),
+        ],
+      ),
+    );
+  }
+}
